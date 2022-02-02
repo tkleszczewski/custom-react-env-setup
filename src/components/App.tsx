da8ffd02eb2812f985a2feefdce8eae41f1b5dab
Tomasz Kleszczewski <tomekkleszczewski@gmail.com>
2022-02-02 18:43:36 +0100

    Add application logic

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,46 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 
 const App = () => {
-  return <h1>App is working</h1>;
+  const [count, setCount] = useState<number>(0);
+
+  const increment = useCallback(() => {
+    setCount((prevCount) => prevCount + 1);
+  }, [setCount]);
+
+  const decrement = useCallback(() => {
+    setCount((prevCount) => prevCount - 1);
+  }, [setCount]);
+
+  return (
+    <div>
+      <main className="main-container">
+        <header className="app-header">
+          <h1 className="app-heading">
+            React application with some logic to test
+          </h1>
+        </header>
+        <section className="counter-section">
+          <div className="content-centered">
+            <div className="count-indicator content-centered">{count}</div>
+            <button
+              className="increment-button"
+              onClick={increment}
+              type="button"
+            >
+              increment counter
+            </button>
+            <button
+              className="increment-button"
+              onClick={decrement}
+              type="button"
+            >
+              decrement counter
+            </button>
+          </div>
+        </section>
+      </main>
+    </div>
+  );
 };
 
 export default App;
